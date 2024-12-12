af46c380fbb48c9428c1ddc8d650784f35b310bf
gagan0024 <43841619+gagan0024@users.noreply.github.com>
2024-12-12 21:15:56 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to My React App</h1>
+      <p>This is a simple React application running in Docker!</p>
+    </div>
+  );
+}
+
+export default App;
